43dc5516cd2d724efcfabb69eb1b4517702ebf10
laxminara1996 <laxminara1996@gmail.com>
2023-08-29 13:35:12 +0530

    initial commit

---
+++ src/main/java/in/laxmi/rest/WelcomeRestController.java
@@ -0,0 +1,16 @@
+package in.laxmi.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+	@Value("${msg}")
+	private String msg;
+
+	@GetMapping("/welcome")
+	public String welcomeMsg() {
+		return msg;
+	}
+}
---
+++ src/test/java/in/laxmi/SbmsWelcomeApiConfigServerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package in.laxmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsWelcomeApiConfigServerClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
